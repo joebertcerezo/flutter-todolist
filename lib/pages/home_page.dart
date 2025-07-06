423533b7474d17c71be37440b8d20879bed41282
sirezo <joebertcerezo@gmail.com>
2025-07-06 18:30:08 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,16 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   final List<ToDoItem> todoList = [
-    ToDoItem("Wash Dishes", false),
-    ToDoItem("Do Laundry", true),
-    ToDoItem("Buy Groceries", false),
-    ToDoItem("Walk the Dog", false),
+    ToDoItem(title: "Wash Dishes", isCompleted: false),
+    ToDoItem(title: "Walk the Dog", isCompleted: true),
+    ToDoItem(title: "Buy Groceries", isCompleted: false),
   ];
 
   void checkBoxChanged(int index) {
     setState(() {
       todoList[index].isCompleted = !todoList[index].isCompleted;
+    });
+  }
+
+  void saveTask() {
+    setState(() {
+      todoList.add(ToDoItem(title: _controller.text));
+      _controller.clear();
     });
   }
 
@@ -39,34 +46,49 @@ class _HomePageState extends State<HomePage> {
           );
         },
       ),
-      floatingActionButton: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color(0xFFEAD8A4),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFFF68537)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xFFF68537)),
-                    borderRadius: BorderRadius.circular(10),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: "Add more todo...",
+                    filled: true,
+                    fillColor: Color(0xFFEAD8A4),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFF68537)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFF68537)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          FloatingActionButton(
-            onPressed: () => {},
-            backgroundColor: Color(0xFFF68537),
-            child: Icon(Icons.add, size: 30),
-          ),
-        ],
+            FloatingActionButton(
+              onPressed: saveTask,
+              backgroundColor: Color(0xFFF68537),
+              child: Icon(Icons.add, size: 30),
+            ),
+          ],
+        ),
       ),
+      // bottomSheet: BottomSheet(
+      //   onClosing: () => {},
+      //   builder: (BuildContext context) {
+      //     return Container(
+      //       height: 50,
+      //       color: const Color(0xFFEAD8A4),
+      //       child: const Center(child: Text('Made by: Joebert L. Cerezo')),
+      //     );
+      //   },
+      // ),
     );
   }
 }
